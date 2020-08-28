75fa200ac3b3cb5797ac631eb7702d180e16cc57
unknown <Shellynguyen98@gmail.com>
2020-08-27 22:35:53 -0700

    update

--- public/app.js
+++ public/app.js
@@ -27,19 +27,16 @@ document.getElementById('addBurger').addEventListener('click', event => {
   document.addEventListener('click', event => {
     if (event.target.classList.contains('devoured')) {
       axios.put(`/api/burgers/${event.target.parentNode.parentNode.id}`, {
-        purchased: true
+        devoured: true
       })
         .then(() => {
           let burgerElem = document.createElement('li')
           burgerElem.className = 'list-group-item'
           burgerElem.id = event.target.parentNode.parentNode.id
           burgerElem.innerHTML = `
-         <div class="d-flex w-100 justify-content-between">
-           <h5 class="mb-1">${event.target.dataset.name}</h5>
-           <button class="btn btn-danger remove">X</button>
-         </div>
+         ${event.target.dataset.name}
         `
-          document.getElementById('devoured').append(groceryElem)
+          document.getElementById('devoured').append(burgerElem)
           event.target.parentNode.parentNode.remove()
         })
         .catch(err => console.error(err))
